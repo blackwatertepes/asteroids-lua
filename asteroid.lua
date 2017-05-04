b26b8c1f7940ccb24c084c5b18640efb32227245
Tyler J. Kuhn <tyler.kuhn@crowdflower.com>
2017-05-03 22:58:06 -0700

    add many asteroids

--- asteroid.lua
+++ asteroid.lua
@@ -1,12 +1,27 @@
-Class = require('util/hump/class')
+Class = require('util/hump.class')
 
 Asteroid = Class{
-  init = function(self, x, y)
+  init = function(self, x, y, velocity)
     self.x = x
     self.y = y
+    self.velocity = velocity
+    -- TODO: This is calculated incorrectly!
+    self.velocityX = velocity * love.math.random()
+    if love.math.random() > .5 then
+      self.velocityX = -self.velocityX
+    end
+    self.velocityY = velocity - self.velocityX
+    if love.math.random() > .5 then
+      self.velocityY = -self.velocityY
+    end
+  end;
+
+  update = function(self)
+    self.x = self.x + self.velocityX
+    self.y = self.y + self.velocityY
   end;
 
   draw = function(self)
     love.graphics.rectangle('fill', self.x, self.y, 20, 20)
-  end
+  end;
 }
--- main.lua
+++ main.lua
@@ -1,20 +1,30 @@
 dev = require('dev') -- Development
+tick = require('util/tick')
 require('asteroid')
+
+asteroids = {}
 
 function love.load()
   dev.load()
-
-  asteroid = Asteroid(100, 100)
+  tick.framerate = 60
 end
 
 function love.update(dt)
   dev.update(dt)
 
-  asteroid.x = asteroid.x + 1
+  if love.math.random() < 0.1 then
+    table.insert(asteroids, Asteroid(love.math.random(love.graphics.getWidth()), love.math.random(love.graphics.getHeight()), 1))
+  end
+
+  for i, asteroid in pairs(asteroids) do
+    asteroid:update()
+  end
 end
 
 function love.draw()
   dev.draw()
 
-  asteroid:draw()
+  for i, asteroid in pairs(asteroids) do
+    asteroid:draw()
+  end
 end
