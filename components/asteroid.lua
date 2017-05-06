5a5ddc685950687467794f52062383ad4e3fb415
Tyler J. Kuhn <tyler.kuhn@crowdflower.com>
2017-05-05 19:43:26 -0700

    drew lil asteroids

--- components/asteroid.lua
+++ components/asteroid.lua
@@ -1,11 +1,16 @@
 local Asteroid = Component.create('Asteroid')
 function Asteroid:initialize()
-  local dist, maxRot = love.graphics.getWidth() * .7, 4
+  local dist, maxRot, complexity = love.graphics.getWidth() * .7, 4, 20
+  -- Find a random starting location on a circle, with a pref for the left and right sides
   local distY = math.random(dist)
   if math.random() > .5 then distY = -distY end
   local distX = math.sqrt(math.pow(dist, 2) - math.pow(distY, 2))
   if math.random() > .5 then distX = -distX end
-
+  -- Generate a set of randos for drawing the poly
+  self.rands = {}
+  for i=0, complexity * 2, 1 do
+    self.rands[i] = math.random(80, 100) / 100
+  end
   self.size = 80
   self.stepRot = math.random() * maxRot - maxRot / 2
   self.targetX = love.graphics.getWidth() / 2 - self.size / 2
--- systems/AsteroidDrawSystem.lua
+++ systems/AsteroidDrawSystem.lua
@@ -1,6 +1,7 @@
 AsteroidDrawSystem = class('AsteroidDrawSystem', System)
 
 function AsteroidDrawSystem:draw()
+  local per, jag = .7 -- percentage to bounding box
   for i, entity in pairs(self.targets) do
     local comp = entity.components.Asteroid
     local x, y, size, rot = comp.x, comp.y, comp.size, comp.rot
@@ -8,11 +9,16 @@ function AsteroidDrawSystem:draw()
       love.graphics.translate(x + size / 2, y + size / 2)
       love.graphics.push()
         love.graphics.rotate(comp.rot)
-        love.graphics.rectangle('line', -size / 2, -size / 2, size, size)
-        love.graphics.push()
-          love.graphics.rotate(1)
-          love.graphics.rectangle('line', -size / 2, -size / 2, size, size)
-        love.graphics.pop()
+        local lastXY = {x = comp.size * per * comp.rands[0], y = comp.rands[1]}
+        for i=1, #comp.rands/2-1, 1 do
+          local theta = math.pi*2 * i/#comp.rands*2
+          x = 0 + comp.size * per * math.cos(theta) * comp.rands[i*2]
+          y = 0 + comp.size * per * math.sin(theta) * comp.rands[i*2+1]
+          love.graphics.line(lastXY.x, lastXY.y, x, y)
+          lastXY.x = x
+          lastXY.y = y
+        end
+        love.graphics.line(lastXY.x, lastXY.y, comp.size * per * comp.rands[0], comp.rands[1])
       love.graphics.pop()
     love.graphics.pop()
     --love.graphics.rectangle('line', comp.x, comp.y, comp.size, comp.size) -- Bounding Box
