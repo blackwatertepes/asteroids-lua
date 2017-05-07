c8327c996901e302cb34b98d67c1f3f83b923cbf
Tyler J. Kuhn <tyler.kuhn@crowdflower.com>
2017-05-07 13:06:50 -0700

    refactored asteroid removal

--- systems/AsteroidUpdateSystem.lua
+++ systems/AsteroidUpdateSystem.lua
@@ -3,28 +3,29 @@ local AsteroidUpdateSystem = class('AsteroidUpdateSystem', System)
 function AsteroidUpdateSystem:update(dt)
   for i, entity in pairs(self.targets) do
     local comp = entity.components.Asteroid
-    goalX = comp.x + (comp.stepX * dt)
-    goalY = comp.y + (comp.stepY * dt)
-    comp.rot = comp.rot + (comp.stepRot * dt)
-    local filter = function(item, other)
-      return other:get('Asteroid') and 'slide'
-    end
-    local actualX, actualY, cols, len = world:move(entity, goalX, goalY, filter)
-    if len == 0 then
-      comp.x = actualX
-      comp.y = actualY
-    else
-      world:remove(entity)
-      engine:removeEntity(entity)
-      for k, col in pairs(cols) do
-        world:remove(col.other)
-        engine:removeEntity(col.other)
-      end
-    end
     -- Remove asteroids that are farther away than the spawn point
     if math.sqrt(math.pow(comp.x, 2) + math.pow(comp.y, 2)) > love.graphics.getWidth() * 3 then
       world:remove(entity)
       engine:removeEntity(entity)
+    else
+      goalX = comp.x + (comp.stepX * dt)
+      goalY = comp.y + (comp.stepY * dt)
+      local filter = function(item, other)
+        return other:get('Asteroid') and 'slide'
+      end
+      local actualX, actualY, cols, len = world:move(entity, goalX, goalY, filter)
+      if len == 0 then
+        comp.rot = comp.rot + (comp.stepRot * dt)
+        comp.x = actualX
+        comp.y = actualY
+      else
+        world:remove(entity)
+        engine:removeEntity(entity)
+        for k, col in pairs(cols) do
+          world:remove(col.other)
+          engine:removeEntity(col.other)
+        end
+      end
     end
   end
 end
